c1997ce6a6bda03de0a1c431e3ef079c4dbfdc44
Kenneth Edoho <kennethtibele@gmail.com>
2024-10-09 12:24:59 +0100

    Add project files.

---
+++ 1ST-CONTACT-ASSIGNMENT/Controllers/HelloController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _1ST_CONTACT_ASSIGNMENT.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello World";
+        }
+
+    }
+}
